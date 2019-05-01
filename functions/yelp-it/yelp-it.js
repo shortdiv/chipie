dec74c4ca2bece3e87c3e5988524efc28c39533e
Divya Sasidharan <hello@shortdiv.com>
2019-05-01 16:58:09 -0500

    debug

--- functions/yelp-it/yelp-it.js
+++ functions/yelp-it/yelp-it.js
@@ -1,5 +1,3 @@
-// const yelp = require("yelp-fusion");
-
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
@@ -8,38 +6,3 @@ exports.handler = function(event, context, callback) {
     })
   });
 };
-
-// exports.handler = function(event, context, callback) {
-//   console.log(process.env.VUE_APP_YELP_API);
-// const apiKey = `${process.env.VUE_APP_YELP_API}`;
-
-// const client = yelp.client(apiKey);
-
-// const { term, location } = event.queryStringParameters;
-
-// const searchRequest = {
-//   term: term,
-//   location: location
-// };
-
-// var fetchFromYelp = async function() {
-//   try {
-//     let res = await client.search(searchRequest);
-// callback(null, {
-//   statusCode: 200,
-//   body: JSON.stringify({
-//     results: "hello"
-//   })
-// });
-//   } catch (err) {
-//     callback(null, {
-//       statusCode: 200,
-//       body: JSON.stringify({
-//         err: err
-//       })
-//     });
-//   }
-// };
-
-// fetchFromYelp();
-// };
