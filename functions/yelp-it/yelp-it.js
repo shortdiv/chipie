d046d8e94cbcba2ffac6dd8e0f278c9e21fdffce
Divya Sasidharan <hello@shortdiv.com>
2019-05-01 16:47:36 -0500

    debug

--- functions/yelp-it/yelp-it.js
+++ functions/yelp-it/yelp-it.js
@@ -1,35 +1,46 @@
-const yelp = require("yelp-fusion");
+// const yelp = require("yelp-fusion");
 
 exports.handler = function(event, context, callback) {
-  const apiKey = `${process.env.VUE_APP_YELP_API}`;
-
-  const client = yelp.client(apiKey);
-
-  const { term, location } = event.queryStringParameters;
-
-  const searchRequest = {
-    term: term,
-    location: location
-  };
-
-  var fetchFromYelp = async function() {
-    try {
-      let res = await client.search(searchRequest);
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          results: res
-        })
-      });
-    } catch (err) {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          err: err
-        })
-      });
-    }
-  };
-
-  fetchFromYelp();
+  console.log(process.env.VUE_APP_YELP_API);
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg: "Hello, World! " + Math.round(Math.random() * 10)
+    })
+  });
 };
+
+// exports.handler = function(event, context, callback) {
+//   console.log(process.env.VUE_APP_YELP_API);
+// const apiKey = `${process.env.VUE_APP_YELP_API}`;
+
+// const client = yelp.client(apiKey);
+
+// const { term, location } = event.queryStringParameters;
+
+// const searchRequest = {
+//   term: term,
+//   location: location
+// };
+
+// var fetchFromYelp = async function() {
+//   try {
+//     let res = await client.search(searchRequest);
+// callback(null, {
+//   statusCode: 200,
+//   body: JSON.stringify({
+//     results: "hello"
+//   })
+// });
+//   } catch (err) {
+//     callback(null, {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         err: err
+//       })
+//     });
+//   }
+// };
+
+// fetchFromYelp();
+// };
