f2cf4b59d2d638b1b91e11de7f88b1cf45ed4df3
Divya Sasidharan <hello@shortdiv.com>
2019-05-01 16:27:51 -0500

    yelp error

--- functions/yelp-it/yelp-it.js
+++ functions/yelp-it/yelp-it.js
@@ -1,8 +1,7 @@
 const yelp = require("yelp-fusion");
 
 exports.handler = function(event, context, callback) {
-  const apiKey =
-    "E9ws6kbLaQqKYFbjcT67QKcGa97K6X4TQg5OQCV6LKsGksRIFuj9ueQg9BCHHIYy5bnTmXPgHaq60f2clOmebW_1p_342TY4Q92gLsw_k8W8joF39wGNhYb3SKOxWXYx";
+  const apiKey = `${VUE_APP_YELP_API}`;
 
   const client = yelp.client(apiKey);
 
@@ -12,37 +11,6 @@ exports.handler = function(event, context, callback) {
     term: term,
     location: location
   };
-
-  // var delay = function(responseItem) {
-  //   return new Promise(resolve => {
-  //     setTimeout(() => {
-  //       resolve({
-  //         type: "Feature",
-  //         geometry: {
-  //           type: "Point",
-  //           coordinates: [
-  //             responseItem.coordinates.longitude,
-  //             responseItem.coordinates.latitude
-  //           ]
-  //         },
-  //         properties: {
-  //           ...responseItem
-  //         }
-  //       });
-  //     }, 1000);
-  //   });
-  // };
-
-  // var geojsonify = async function() {
-  //   let items = await client.search(searchRequest);
-  //   const noOfItems = items.length;
-
-  // for(var i = 0; i < noOfItems; i++) {
-  //   const orderPromise = delay(items[i])
-  //   promises.push(orderPromise)
-  // }
-  // return Promise.all(promises)
-  // };
 
   var fetchFromYelp = async function() {
     try {
